bff039c0ed1b9617cb19356aa3c0baa684b65908
ebin6 <ebin.oneteam@gmail.com>
2026-06-01 12:40:31 +0530

    Created basic edit page for movie and displayed current values . Edit changes saving pending

---
+++ movies/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from movies import views
+
+
+app_name="movies"
+urlpatterns=[
+    path("",views.index,name="home"),
+    path("add-movie",views.addMovie,name="add_movie"),
+    path("all-movies",views.allMovies,name="all_movies"),
+    path("edit-movie/<int:movie_id>",views.editMovie,name="edit_movie")
+]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from movies.models import Movie
 # Create your views here.
+
+def index(request):
+    return render(request,"index.html")
+
+def addMovie(request):
+    if request.method=="POST":
+        movie_name=request.POST["title"]
+        desc=request.POST["description"]
+        image=request.FILES["poster"]
+        date=request.POST["release_date"]
+        tr_link=request.POST["trailer_link"]
+        movie=Movie(name=movie_name,poster=image,description=desc,trailer_link=tr_link,release_date=date)
+        movie.save()
+        return redirect("movies:all_movies")
+    return render(request,"add-movie.html")
+
+def allMovies(request):
+    movies=Movie.objects.all()
+    return render(request,"all-movies.html",{"movies":movies})
+
+
+def editMovie(request,movie_id):
+    movie=Movie.objects.get(id=movie_id)
+    return render(request,"edit-movie.html",{"movie":movie})
